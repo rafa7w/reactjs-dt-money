fa7a90521eafc93cf14a0c5b4842afe9407120a1
Rafael William <rafael.fim@hotmail.com>
2023-08-09 13:36:00 -0300

    calculate transactions summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,6 +5,23 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+
+    return acc
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0
+  })
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -12,15 +29,15 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e"/>
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
-      <SummaryCard>
+      <SummaryCard> 
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68"/>
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -28,7 +45,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff"/>
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
