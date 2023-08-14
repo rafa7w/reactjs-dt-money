f1722c8aa908bd2c095ee03fc7e0b3b3074b8f6f
Rafael William <rafael.fim@hotmail.com>
2023-08-14 14:10:03 -0300

    applying useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -62,7 +62,7 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
